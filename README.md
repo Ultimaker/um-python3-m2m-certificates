c62fdff12ea99dc7c161baea61c68de021128b4d
Loy van Beek <l.vanbeek@ultimaker.com>
2018-01-26 13:24:12 +0100

    Initial commit
    EM-2079

---
+++ README.md
@@ -0,0 +1,2 @@
+This repository's purpose is to create a debian package of the m2m_certificates
+python package (https://github.com/LoyVanBeek/m2m_certificates).
---
+++ build_for_ultimaker.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+SRC_PATH="$(pwd)"
+MODULE_PATH="$(pwd)/m2m_certificates"
+
+init() {
+	git submodule update --init --recursive
+}
+
+build() {
+	cd "${MODULE_PATH}" &&
+	python3 setup.py --command-packages=stdeb.command bdist_deb &&
+	cp deb_dist/python3-m2m_certificates*.deb "${SRC_PATH}/"
+	cd "${SRC_PATH}"
+}
+
+init
+build
